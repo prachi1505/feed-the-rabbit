67178b0fdfc7f44069db4cb301e9e16f2ee05b2f
prachi1505 <85737452+prachi1505@users.noreply.github.com>
2021-07-24 15:57:41 +0530

    Add files via upload

---
+++ Project 12/sketch.js
@@ -0,0 +1,83 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple,appleImg;
+var leaves,leavesImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leavesImg = loadImage("leaf.png");
+
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+
+
+
+
+
+
+
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  rabbit.x=mouseX
+  
+  var select = Math.round(random(1,2))
+  if(frameCount % 80===0){
+    if(select===1){
+      creatApples()
+    }
+    else{
+      creatLeaves()
+    }
+  }
+
+  drawSprites();
+
+}
+
+
+function creatApples(){
+
+  apple = createSprite(random(50,350),40,10,10);
+apple.addImage(appleImg)
+apple.scale=0.1;
+apple.velocityY=1;
+
+
+
+
+
+  
+}
+function creatLeaves(){ 
+  leaves= createSprite(random(50,350),40,10,10);
+  leaves.addImage(leavesImg)
+  leaves.scale=0.1
+  leaves.velocityY=1
+}
+
+
+
+
+
